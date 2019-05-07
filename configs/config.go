e49c77d7dfa9ad3e74577f6f7b4ad0605fd9b1a8
champly <champly@outlook.com>
2019-05-07 18:37:13 +0800

    添加config(未完成)

--- configs/config.go
+++ configs/config.go
@@ -1,8 +1,48 @@
 package configs
 
+import (
+	"github.com/spf13/viper"
+)
+
 type ServerConfig struct {
 	PlatName   string `json:"plat_name"`
 	SystemName string `json:"system_name"`
 	Addr       string `json:"addr"`
 	Mode       string `json:"mode"`
+}
+
+type plat struct {
+	Name string `json:"name"`
+}
+
+var PlatInfo = &plat{}
+
+type system struct {
+	Name string `json:"name"`
+	Mode string `json:"mode"`
+}
+
+var SystemInfo = &system{}
+
+type httpserver struct {
+	CorsConfig httpcors `json:"cors_config"`
+	Address    string   `json:"address"`
+}
+
+type httpcors struct {
+	NeedCors bool                `json:"need_cors"`
+	Header   []map[string]string `json:"header"`
+}
+
+var HttpServerInfo = &httpserver{}
+
+type log struct {
+	LogLevel string `json:"log_level"`
+	Out      string `json:"out"`
+}
+
+var LogInfo = &log{}
+
+func Setup() {
+	viper.Get("plat")
 }
--- init/init.go
+++ init/init.go
@@ -1,6 +1,31 @@
 package init
 
 import (
+	"fmt"
+
 	_ "github.com/champly/hercules/servers/cron"
 	_ "github.com/champly/hercules/servers/http"
+	"github.com/spf13/viper"
 )
+
+func init() {
+	fmt.Println("init execute")
+	initConfig()
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	viper.AddConfigPath("$HOME/config")
+	viper.AddConfigPath("./")
+	viper.AddConfigPath("./config")
+	viper.SetConfigName("config")
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err != nil {
+		// panic("load config file error:" + err.Error())
+		return
+	}
+
+	fmt.Println("Using config file:", viper.ConfigFileUsed())
+	return
+}
